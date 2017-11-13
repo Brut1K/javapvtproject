9b50b6f4cb3e564a3c24b0e75b1697b6cd77cd8c
Brut1K <brutski.aliaksei@gmail.com>
2017-11-13 13:27:12 +0300

    ff

--- src/HworkLesson1.java
+++ src/HworkLesson1.java
@@ -1,13 +1,15 @@
 public class HworkLesson1 {
     public static void main(String[] args) {
+
         String name = "This is not short text, this is a long-long text!";
-        System.out.println("Количество символов в нашей строке "+
-                name.length());
-        int numsymbols = name.length();
-        int half = numsymbols/2;
+        System.out.println("Количество символов в нашей строке " + name.length());
+        int half = name.length()/2;
         String firsthalf = name.substring(0,half);
         String secondhalf = name.substring(half);
-        System.out.println(firsthalf+firsthalf.length());
-        System.out.println(secondhalf+secondhalf.length());
+        System.out.println("Первая половина строки длиной " + firsthalf.length() + ": " + firsthalf);
+        System.out.println("Вторая половина строки длиной " + secondhalf.length() + ": " + secondhalf);
+
+        
+
     }
 }
