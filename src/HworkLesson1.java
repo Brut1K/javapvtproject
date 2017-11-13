9260eebcbfcffb9b0e2724aaab1d0ce8b8f69a21
Brut1K <brutski.aliaksei@gmail.com>
2017-11-13 16:49:45 +0300

    l1t5

--- src/HworkLesson1.java
+++ src/HworkLesson1.java
@@ -40,6 +40,14 @@ public class HworkLesson1 {
         }
 
 
+        String l1t5 = "hkj?h1#238u?kasda##r12?#3.?@K2lj##jh323";
+
+        l1t5 = l1t5.replace("?","HELLO");
+        l1t5 = l1t5.replace("#","");
+
+        System.out.println(l1t5);
+
+
     }
 
 
