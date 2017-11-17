503ab5656ffb39dfb1c89f641682753b6c743602
Brut1K <brutski.aliaksei@gmail.com>
2017-11-17 16:21:51 +0300

    HW2 summary

--- src/HworkLesson1.java
+++ src/HworkLesson1.java
@@ -33,10 +33,13 @@ public class HworkLesson1 {
         int l1t4 = 187236120;
         String l1t4string = Integer.toString(l1t4);
         char l1t4char = l1t4string.charAt(l1t4string.length() - 1);
-        if (l1t4char == '1' || l1t4char == '2' || l1t4char == '3' || l1t4char == '4') {
-            System.out.println(l1t4 + " рубля");
-        } else {
-            System.out.println(l1t4 + " рублей");
+        if(l1t4char=='1') {
+            System.out.println(l1t4+ " рубль");
+        } else if (l1t4char == '2' || l1t4char == '3' || l1t4char == '4'&&l1t4>20) {
+                System.out.println(l1t4 + " рубля");
+            } else {
+                System.out.println(l1t4 + " рублей");
+            }
         }
 
 
