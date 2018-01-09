c7cedcd7cd55e2e085d9564fa986d983505eb69d
Brut1K <brutski.aliaksei@gmail.com>
2018-01-09 17:41:21 +0300

    HW13 final

--- HomeWork/Homework13/src/by/it_academy/task2/Main.java
+++ HomeWork/Homework13/src/by/it_academy/task2/Main.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String file = "test_22.doc";
-        String p = "([\\wА-яа-я]{1,})(\\.)(\\w+$)";
+        String file = "test_тест22.doc";
+        String p = "([\\wА-яа-я]{1,})(.)(\\w+$)";
         Pattern pattern = Pattern.compile(p);
         Matcher m = pattern.matcher(file);
         if(m.matches()) {
